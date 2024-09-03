5997bbd578bea9e5a813b4df72bd7488d2fe5f5a
KamogeloMahlake <3949843@myuwc.ac.za>
2024-09-03 10:43:51 +0200

    create the rectangle and square

--- polygon-area-calculator/area_calculator.py
+++ polygon-area-calculator/area_calculator.py
@@ -26,26 +26,43 @@ class Rectangle:
             return "Too big for picture."
         
         else:
-            picture = ''
-            for i in range(self.height):
-                picture += "*" * self.width + "\n"
-                
-            return picture
+            picture = []
+            row = ""
+            for i in range( 0,self.height):
+                for i in range(self.width):
+                    row = ("*" * self.width)
+                picture.append(row)
+            shape = "\n".join(picture)
+        return shape + "\n"
         
     def get_amount_inside(self, shape):
         if (self.get_area()) >= shape.get_area():
             return self.get_area() // shape.get_area()
         return 0
 
-class Square(Rectangle):    
+class Square(Rectangle):
+    
     def __init__(self, side):
         super().__init__(side, side)
 
     def set_side(self, side):
         self.width = side
-        self.heigth = side
+        self.height = side
+
+    def set_width(self, side):
+        self.width = side
+        self.height = side
+
+    def set_height(self, side):
+        self.width = side
+        self.height = side
 
     def __str__(self):
         return f"Square(side={self.width})"
+
+
+
+
+
 
 
