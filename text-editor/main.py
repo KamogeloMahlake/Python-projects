4c1b766bea0c51abfa3d570c6d694b4591aa5f7e
KamogeloMahlake <3949843@myuwc.ac.za>
2025-03-13 15:35:31 +0200

    feat: add font size option

--- text-editor/main.py
+++ text-editor/main.py
@@ -1,10 +1,20 @@
-from tkinter import Tk, Text, filedialog, Menu
+from tkinter import (
+    Tk,
+    Text,
+    filedialog,
+    Menu,
+)
 import tkinter as tk
+
+from functools import partial
 
 
 class TextEditor:
     def __init__(self):
         self.current_file_path = ""
+        self.current_font = "Arial"
+        self.current_font_size = 12
+
         self.root = Tk()
         self.root.title("Untitled - Text Editor")
 
@@ -16,6 +26,7 @@ class TextEditor:
 
         self.filemenu = Menu(self.menu)
         self.menu.add_cascade(label="File", menu=self.filemenu)
+        self.filemenu.add_command(label="New", command=self.__new)
         self.filemenu.add_command(label="Open...", command=self.__open)
         self.filemenu.add_command(label="Save", command=self.__save)
         self.filemenu.add_command(label="Save as", command=self.__save_as)
@@ -26,11 +37,25 @@ class TextEditor:
         self.menu.add_cascade(label="Format", menu=self.formatmenu)
         self.font = Menu(self.formatmenu)
         self.formatmenu.add_cascade(label="Font", menu=self.font)
-        self.font.add_command(label="Courier", command=self.__font_courier)
-        self.font.add_command(label="Helvetica", command=self.__font_helvetica)
+        self.font_size = Menu(self.formatmenu)
+        self.formatmenu.add_cascade(label="Font Size", menu=self.font_size)
+        self.fonts = ["Arial", "Courier", "Times New Roman", "Roman", "Comic Sans"]
+        self.font_sizes = [10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30]
+
+        for font in self.fonts:
+            self.font.add_command(label=font, command=partial(self.__font, font))
+        for size in self.font_sizes:
+            self.font_size.add_command(
+                label=str(size), command=partial(self.__font_size, size)
+            )
 
     def run(self):
         self.root.mainloop()
+
+    def __new(self):
+        self.text.delete(1.0, tk.END)
+        self.current_file_path = ""
+        self.root.title("Untitled - Text Editor")
 
     def __update_title(self, new_title):
         self.root.title(f"{new_title.split('/')[-1].split('.')[0]} - Text Editor")
@@ -54,27 +79,28 @@ class TextEditor:
             self.current_file_path = path
 
     def __save_as(self):
-
-        path = filedialog.asksaveasfile(
+        path = filedialog.asksaveasfilename(
             defaultextension=".txt", filetypes=[("Text files", "*.txt")]
         )
         if path:
-            path.write(self.text.get(1.0, tk.END))
-            path.close()
-            self.__update_title(path)
-            self.current_file_path = path
+            with open(path, "w") as file:
+                file.write(self.text.get(1.0, tk.END))
+                self.__update_title(path)
+                self.current_file_path = path
 
         else:
             self.__save_as()
-
         return
 
-    def __font_courier(self):
-        self.text.config(font="Courier")
+    def __font(self, font):
+        self.text.config(font=(font, self.current_font_size))
+        self.current_font = font
 
-    def __font_helvetica(self):
-        self.text.config(font="Helvetica")
-    
+    def __font_size(self, size):
+        self.text.config(font=(self.current_font, size))
+        self.current_font_size = size
+
+
 if __name__ == "__main__":
     text_editor = TextEditor()
     text_editor.run()
