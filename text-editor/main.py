0a814e9a4fd0cd4d0cd5ae93a9b9b08dcfaa0683
Kamogelo Mahlake <3949843@myuwc.ac.za>
2025-03-17 09:13:17 +0000

    feat: add error handling

--- text-editor/main.py
+++ text-editor/main.py
@@ -1,11 +1,5 @@
-from tkinter import (
-    Tk,
-    Text,
-    filedialog,
-    Menu,
-)
+from tkinter import Tk, Text, filedialog, Menu
 import tkinter as tk
-
 from functools import partial
 
 
@@ -27,74 +21,88 @@ class TextEditor:
         self.menu = Menu(self.root)
         self.root.config(menu=self.menu)
 
-        self.filemenu = Menu(self.menu)
-        self.menu.add_cascade(label="File", menu=self.filemenu)
-        self.filemenu.add_command(label="New", command=self.__new)
-        self.filemenu.add_command(label="Open...", command=self.__open)
-        self.filemenu.add_command(label="Save", command=self.__save)
-        self.filemenu.add_command(label="Save as", command=self.__save_as)
-        self.filemenu.add_separator()
-        self.filemenu.add_command(label="Exit", command=self.root.quit)
-
-        self.formatmenu = Menu(self.menu)
-        self.menu.add_cascade(label="Format", menu=self.formatmenu)
-        self.font = Menu(self.formatmenu)
-        self.formatmenu.add_cascade(label="Font", menu=self.font)
-        self.font_size = Menu(self.formatmenu)
-        self.formatmenu.add_cascade(label="Font Size", menu=self.font_size)
-        
-
-        for font in self.fonts:
-            self.font.add_command(label=font, command=partial(self.__font, font))
-        for size in self.font_sizes:
-            self.font_size.add_command(
-                label=str(size), command=partial(self.__font_size, size)
-            )
+        self.create_file_menu()
+        self.create_format_menu()
 
     def run(self):
         self.root.mainloop()
 
-    def __new(self):
+    def create_file_menu(self):
+        filemenu = Menu(self.menu)
+        self.menu.add_cascade(label="File", menu=filemenu)
+        filemenu.add_command(label="New", command=self.new_file)
+        filemenu.add_command(label="Open...", command=self.open_file)
+        filemenu.add_command(label="Save", command=self.save_file)
+        filemenu.add_command(label="Save as", command=self.save_file_as)
+        filemenu.add_separator()
+        filemenu.add_command(label="Exit", command=self.root.quit)
+
+    def create_format_menu(self):
+        formatmenu = Menu(self.menu)
+        self.menu.add_cascade(label="Format", menu=formatmenu)
+        font_menu = Menu(formatmenu)
+        formatmenu.add_cascade(label="Font", menu=font_menu)
+        font_size_menu = Menu(formatmenu)
+        formatmenu.add_cascade(label="Font Size", menu=font_size_menu)
+
+        for font in self.fonts:
+            font_menu.add_command(label=font, command=partial(self.set_font, font))
+        for size in self.font_sizes:
+            font_size_menu.add_command(
+                label=str(size), command=partial(self.set_font_size, size)
+            )
+
+    def new_file(self):
         self.text.delete(1.0, tk.END)
         self.current_file_path = ""
         self.root.title("Untitled - Text Editor")
 
-    def __update_title(self, new_title):
+    def update_title(self, new_title):
         self.root.title(f"{new_title.split('/')[-1].split('.')[0]} - Text Editor")
 
-    def __save(self):
+    def save_file(self):
         if self.current_file_path:
-            with open(self.current_file_path, "w") as file:
-                file.write(self.text.get(1.0, tk.END))
+            try:
+                with open(self.current_file_path, "w") as file:
+                    file.write(self.text.get(1.0, tk.END))
+            except Exception as e:
+                print(f"Error saving file: {e}")
         else:
-            self.__save_as()
+            self.save_file_as()
 
-    def __open(self):
+    def open_file(self):
         path = filedialog.askopenfilename(
             defaultextension=".txt", filetypes=[("Text files", "*.txt")]
         )
-        with open(path, "r") as file:
-            content = file.read()
-            self.text.delete(1.0, tk.END)
-            self.text.insert(tk.INSERT, content)
-            self.__update_title(path)
-            self.current_file_path = path
+        if path:
+            try:
+                with open(path, "r") as file:
+                    content = file.read()
+                    self.text.delete(1.0, tk.END)
+                    self.text.insert(tk.INSERT, content)
+                    self.update_title(path)
+                    self.current_file_path = path
+            except Exception as e:
+                print(f"Error opening file: {e}")
 
-    def __save_as(self):
+    def save_file_as(self):
         path = filedialog.asksaveasfilename(
             defaultextension=".txt", filetypes=[("Text files", "*.txt")]
         )
         if path:
-            with open(path, "w") as file:
-                file.write(self.text.get(1.0, tk.END))
-                self.__update_title(path)
-                self.current_file_path = path
+            try:
+                with open(path, "w") as file:
+                    file.write(self.text.get(1.0, tk.END))
+                    self.update_title(path)
+                    self.current_file_path = path
+            except Exception as e:
+                print(f"Error saving file as: {e}")
 
-    def __font(self, font):
+    def set_font(self, font):
         self.text.config(font=(font, self.current_font_size))
         self.current_font = font
 
-    def __font_size(self, size):
+    def set_font_size(self, size):
         self.text.config(font=(self.current_font, size))
         self.current_font_size = size
 
